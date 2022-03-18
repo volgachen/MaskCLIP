2987d109ac6ad0398944d09f3c5475a6e7bc96a3
chongzhou <cczhou@ucdavis.edu>
2022-03-18 16:09:12 +0800

    fcn on vit16

---
+++ configs/fcn/fcn_vit16_p100_480x480_40k_pascal_context_59.py
@@ -0,0 +1,12 @@
+_base_ = './fcn_vit16_480x480_40k_pascal_context_59.py'
+model = dict(
+    backbone=dict(
+        freeze_xsfm=True,
+        freeze_cls=True,
+        freeze_pos=True,
+        num_prompt=100,
+    ),
+)
+# data = dict(
+#     samples_per_gpu=2,
+# )
