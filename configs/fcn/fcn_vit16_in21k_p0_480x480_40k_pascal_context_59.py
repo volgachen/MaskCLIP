61d5383ce28f42dbc78aed51c2b786282b63450c
Chong Zhou <chongzhou1024@gmail.com>
2022-05-26 16:11:36 +0800

    remove PhotoMetricDistortion for robustness baselines

--- configs/fcn/fcn_vit16_in21k_p0_480x480_40k_pascal_context_59.py
+++ configs/fcn/fcn_vit16_in21k_p0_480x480_40k_pascal_context_59.py
@@ -2,3 +2,23 @@ _base_ = './fcn_vit16_p0_480x480_40k_pascal_context_59.py'
 model = dict(
     pretrained='pretrain/vit-base-p16.pth',
 )
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_scale = (520, 520)
+crop_size = (480, 480)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', reduce_zero_label=True),
+    dict(type='Resize', img_scale=img_scale, ratio_range=(0.5, 2.0)),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type='RandomFlip', prob=0.5),
+    # dict(type='PhotoMetricDistortion'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size=crop_size, pad_val=0, seg_pad_val=255),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_semantic_seg']),
+]
+data = dict(
+    train=dict(pipeline=train_pipeline)
+)
